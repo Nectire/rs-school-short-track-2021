39696760746f2ae46e19ca216467c35beffccc5f
Maxim Yazykov <yazykovmaxim@gmail.com>
2021-04-12 16:07:52 +0300

    feat: add solution to 4 task

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,28 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+
+function minesweeper(matrix) {
+  let newArr = [];
+  newArr = matrix.map((elw, i) => elw.map((el, a) => {
+    let bombs = 0;
+    if (el !== true) {
+      for (let y = -1; y <= 1; y++) {
+        if (i + y >= 0 && i + y < matrix.length) {
+          for (let x = -1; x <= 1; x++) {
+            if (a + x >= 0 && a + x < elw.length) {
+              if (matrix[i + y][a + x] === true) {
+                bombs++;
+              }
+            }
+          }
+        }
+      }
+      return bombs;
+    }
+    return 1;
+  }));
+  return newArr;
 }
 
 module.exports = minesweeper;
